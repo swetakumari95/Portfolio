dbd67e2c07b30f9e7b7f6cf345a54c4dadbf4240
Sweta Kumari <sweta191995@gmail.com>
2016-10-29 14:38:24 +0530

    added navbar

--- script/script.js
+++ script/script.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
     
     $(window).scroll(function(){
-        $(".header h1, .header h3").css("opacity", 1 - $(window).scrollTop() / 300);
+        if ($(window).scrollTop()==0)
+            $(".navbar").addClass("hide");
+        else
+            $(".navbar").removeClass("hide");
+//        if ($(window).scrollTop()>=$(".header").height())
+//            $(".navbar").css("backgroundColor: #343838; color: white;");
+        $(".header h1, .header h3").css("opacity", 1 - $(window).scrollTop() / 200);
     });
     
 });
