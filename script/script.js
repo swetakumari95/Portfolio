fce0768a0967ea13b76b20ed18027c54c52a9b3b
Sweta Kumari <sweta191995@gmail.com>
2016-10-31 00:47:32 +0530

    Added about me section

--- script/script.js
+++ script/script.js
@@ -1,11 +1,17 @@
 $(document).ready(function(){
     
+    //on scroll events
+    
     $(window).scroll(function(){
+        
+        //initially hidden navbar displays itself on scroll down
         if ($(window).scrollTop()==0)
             $(".navbar").addClass("hide");
         else
             $(".navbar").removeClass("hide");
         
+        //navbar changes theme on leaving the landing page
+    
         if ($(window).scrollTop()>=$(".header").height()){
             $(".navbar").css({"backgroundColor": "#343838"});
             $(".navbar li a").css({"color": "white"});
@@ -14,9 +20,13 @@ $(document).ready(function(){
             $(".navbar li a").css({"color": "#343838"});
         }
         
+        //main header fades out on scroll down, reappers on scroll up
+        
         $(".header h1, .header h3").css("opacity", 1 - $(window).scrollTop() / 200);
         
     });
+    
+    //toggles menu on click of hamburger icon
     
     $("#icon").click(function(){
         if ($(".navbar").hasClass("responsive")){
